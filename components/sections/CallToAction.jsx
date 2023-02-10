0e6cdd94a6d4b9af6892d1b2f3e7e07b405253f9
Zaid Sameer <amrikee7@gmail.com>
2023-02-10 20:09:17 +0400

    changing html entities

--- components/sections/CallToAction.jsx
+++ components/sections/CallToAction.jsx
@@ -7,7 +7,7 @@ const CallToAction = () => {
                   <Container className="flex items-start flex-col md:flex-row justify-between">
                         <div className="mb-10 md:mb-0">
                               <h2 className="section-title text-white mb-2">Interseted?</h2>
-                              <p className="text-white text-lg">Let's Get In Touch and Talk with our sales team</p>
+                              <p className="text-white text-lg">Let&apos;s Get In Touch and Talk with our sales team</p>
                         </div>
 
                         <PrimaryButton link="/contact" className="text-lg mx-0 w-full md:w-auto">Contact us</PrimaryButton>
